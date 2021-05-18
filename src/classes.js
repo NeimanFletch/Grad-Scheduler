4974360f247e12c4eb8cc5d1a35ad24fe24e9589
Andrew Wang <wangnrew@gmail.com>
2021-05-18 19:31:03 -0400

    commit master

---
+++ src/classes.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
+
+const Coreclass = () => {
+  return <div></div>;
+};
+
+class App extends React.Component {}
+export default App;
